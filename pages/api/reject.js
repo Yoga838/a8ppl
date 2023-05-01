abcb18ce4c240cadd711897db4606e5b14f65ab0
Yoga <212410102031@mail.unej.ac.id>
2023-05-01 08:24:25 +0700

    prisma ngukuk

--- pages/api/reject.js
+++ pages/api/reject.js
@@ -4,22 +4,22 @@ import { authMiddleware } from "@/lib/middleware";
 export default authMiddleware(async function handler(req,res){
     if(req.method === "POST"){
         const userId = req.user.userId;
-        // const {name,email,password,id} = req.body
-        // if (!name||!email||!password){
-        //     return res.status(400).json({message: 'form kurang lengkap'})
-        // }
-        const Rejected = await prisma.mitra.findMany({
-            // data:{
-            //     name,
-            //     email,
-            //     password
-            // }
+        const {name,email,password,id} = req.body
+        if (!name||!email||!password){
+            return res.status(400).json({message: 'form kurang lengkap'})
+        }
+        const Reject = await prisma.rejected.create({
+            data:{
+                name,
+                email,
+                password
+            }
         })
-        // const mitra = await prisma.mitra.delete({
-        //     where:{
-        //         id
-        //     }
-        // })
+        const mitra = await prisma.mitra.delete({
+            where:{
+                id
+            }
+        })
         return res.status(200).send({Rejected})
 
         
