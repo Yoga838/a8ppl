b8a968135e26ab455d7fc91464037b6d83d13d14
Yoga <212410102031@mail.unej.ac.id>
2023-05-05 10:52:15 +0700

    home change

--- pages/visitor/index.js
+++ pages/visitor/index.js
@@ -91,7 +91,7 @@ export default function visitor_page() {
               <Link href='/visitor/profil'><div className="circle mt-5" /></Link>
               <h4>{data.name}</h4>
               <div className="button-item d-flex pb-2 flex-column align-items-center gap-4">
-                <button type="button" className="btn btn-admin btn-light poppins rounded-pill shadow btn-lg">Konfirmasi Pendistribusian</button>
+                <button type="button" className="btn btn-admin btn-light poppins rounded-pill shadow btn-lg">Home</button>
                 <button type="button" className="btn btn-admin btn-light poppins rounded-pill shadow btn-lg">Tracking</button>
                 <button onClick={pop} type="button" className="btn btn-admin btn-light poppins rounded-pill shadow btn-lg">Log Out</button>
               </div>
