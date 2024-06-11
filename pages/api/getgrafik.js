dd4ab5e4956c2b94570a070232a1aea24e51577e
Yoga838 <212410102031@mail.unej.ac.id>
2024-06-11 19:02:31 +0700

    fix query

--- pages/api/getgrafik.js
+++ pages/api/getgrafik.js
@@ -7,21 +7,23 @@ export default authMiddleware(async function handler(req,res){
         const userId = req.user.userId;
         const id = Number(userId)
         const data = await prisma.$queryRaw`
-            SELECT
-            p.nama_pencatatan,
-            SUM(dp.pemasukan) AS total_pemasukan,
-            SUM(dp.pengeluaran) AS total_pengeluaran,
-            SUM(dp.saldo) AS total_saldo
-            FROM
-            pencatatan p
-            LEFT JOIN detail_pencatatan dp ON p.id = dp.detail_dari
-            WHERE
-            dp.tanggal > NOW() - INTERVAL '1 year'AND p.milik = ${id}
-            GROUP BY
-            p.id
-            ORDER BY
-            p.nama_pencatatan ASC
-      `
+        SELECT
+          p.nama_pencatatan,
+          SUM(dp.pemasukan) AS total_pemasukan,
+          SUM(dp.pengeluaran) AS total_pengeluaran,
+          SUM(dp.saldo) AS total_saldo
+        FROM
+          pencatatan p
+        LEFT JOIN
+          detail_pencatatan dp ON p.id = dp.detail_dari
+        WHERE
+          dp.tanggal > NOW() - INTERVAL 1 YEAR AND p.milik = ${id}
+        GROUP BY
+          p.id, p.nama_pencatatan
+        ORDER BY
+          p.nama_pencatatan ASC
+      `;
+   
         const resultStringified = JSON.parse(JSON.stringify(data, (key, value) => {
         if (typeof value === 'bigint') {
           return value.toString()
